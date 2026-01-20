2977f5a719e499fe3c006976b6cf4ae0ef9b97d0
Josh Fryer <josh.fryer2@gmail.com>
2026-01-20 19:07:09 +0000

    init

---
+++ functionsApiService.js
@@ -0,0 +1,20 @@
+import * as keys from "./keys.js";
+
+export const addPageViewApi = async () => {
+  const apiUrl = keys.addPageViewUrl;
+
+  const response = await fetch(apiUrl, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+    },
+    cache: "no-store", // for always fresh dynamic data
+  });
+
+  if (!response.ok) {
+    const text = await response.text();
+    throw new Error(`GetPageViews failed (${response.status}): ${text}`);
+  }
+
+  return await response.json();
+};
---
+++ visitorCounter.js
@@ -0,0 +1,27 @@
+import { addPageViewApi } from "./functionsApiService.js";
+
+window.onload = () => {
+  addPageView();
+};
+
+const addPageView = async () => {
+  try {
+    const data = await addPageViewApi();
+
+    if (!data || !data.views) {
+      throw new Error(
+        "Invalid data returned from AddPageView API. No views data.",
+      );
+    }
+
+    updateDisplay(data.views);
+  } catch (err) {
+    console.error(err.message);
+    return 0;
+  }
+};
+
+const updateDisplay = (count) => {
+  const el = document.getElementById("page-view-count");
+  if (el) el.textContent = count;
+};
